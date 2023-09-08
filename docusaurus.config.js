01737d5411476ec9d862282f5c662d56b5e8eb71
dodojie <dodo.jie@163.com>
2023-09-08 11:56:03 +0800

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'dodojie', // Usually your GitHub org/user name.
   projectName: 'dodojie.github.io', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
