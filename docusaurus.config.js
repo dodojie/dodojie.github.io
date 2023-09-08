fa99ab5abd59ec82f41753299743301dd0429b39
dodojie <dodo.jie@163.com>
2023-09-08 11:36:16 +0800

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'dodojie', // Usually your GitHub org/user name.
+  projectName: 'dodojie.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
